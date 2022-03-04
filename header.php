df687c3d62ca78fb2c673bc8af43263ee1800a1a
Taiyousan <shams_1414@yahoo.fr>
2022-03-04 15:15:21 +0100

    ??

--- header.php
+++ header.php
@@ -2,14 +2,14 @@
     <nav id="colorlib-main-nav" role="navigation">
         <a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle active"><i></i></a>
         <div class="js-fullheight colorlib-table">
-            <div class="img" style="background-image: url(images/bg_3.jpg);"></div>
+            <div class="img" id="topmenu"></div>
             <div class="colorlib-table-cell js-fullheight" id="menumenu">
                 <div class="row no-gutters">
-                    <div class="col-md-12 text-center">
+                    <div class="col-md-12 text-center" id="menutest">
 
                         <h1 class="mb-4">
-                            <a href="index.html" class="logo">
-                                <img src="images/logo_texte_blanc.png" alt="Logo WishList">
+                            <a href="index.html" class="logo" id="logologo">
+                                <img src="images/logo_texte_blanc.png" alt="Logo WishList" style="max-width: 150px;">
                             </a>
                         </h1>
                         <ul>
