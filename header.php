dd68da623241ef10625a923c02959348c8b10b3d
Taiyousan <shams_1414@yahoo.fr>
2022-03-04 16:45:01 +0100

    upload vendredi-3

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <div class="page">
     <nav id="colorlib-main-nav" role="navigation">
-        <a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle active"><i></i></a>
+        <a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle active" id="bouton"><i></i></a>
         <div class="js-fullheight colorlib-table">
             <div class="img" id="topmenu"></div>
             <div class="colorlib-table-cell js-fullheight" id="menumenu">
@@ -8,14 +8,14 @@
                     <div class="col-md-12 text-center" id="menutest">
 
                         <h1 class="mb-4">
-                            <a href="index.html" class="logo" id="logologo">
+                            <a href="index.php" class="logo" id="logologo">
                                 <img src="images/logo_texte_blanc.png" alt="Logo WishList" style="max-width: 150px;">
                             </a>
                         </h1>
                         <ul id="liste-nav">
-                            <li class="active"><a href="index.html"><span>Home</span></a></li>
-                            <li><a href="about.html"><span>About</span></a></li>
-                            <li><a href="blog.html"><span>Blog</span></a></li>
+                            <li class="active"><a href="index.php"><span>Accueil</span></a></li>
+                            <li><a href="listing.php"><span>Wishlist</span></a></li>
+                            <li><a href="recherche.php"><span>Recherche</span></a></li>
                             <li><a href="contact.html"><span>Contact</span></a></li>
                         </ul>
 
--- index.php
+++ index.php
@@ -4,9 +4,9 @@ require 'header.php';
 ?>
 <section class="hero-wrap js-fullheight">
     <div class="container px-0">
-        <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center" data-scrollax-parent="true">
+        <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center" data-scrollax-parent="true" id="index-corps">
             <div class="col-md-12 ftco-animate text-center">
-                <div class="desc">
+                <div class="desc" id="index-desc">
                     <h1>WishList</h1>
                     <h3>Votre liste de souhaits personnalisée.</h3>
                 </div>
